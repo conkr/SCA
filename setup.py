518224b667eba28fe39242fdc3ec708a534b77f8
conkr <runnever@gmail.com>
2014-04-14 17:29:20 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='spa',
+    version='0.1',
+    #requires = ['scipy', 'numpy'],
+   # package_data = { 'speckle': ['gpu/kernels/*.cl']},
+    author = "Run Su",
+    author_email = "runnever@gmail.com",
+    packages=['spa'],
+   # install_requires=[ 'numpy','scipy' ],
+    description = "python package for scattering and imaging data analysis at Beamline 12.0.2 at the Advanced Light Source",
+    url = "https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/"
+)
+  
