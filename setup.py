4fc995bd67e2b6f7ed007b8df58672323c47d5e9
conkr <runnever@gmail.com>
2014-04-21 16:13:40 -0700

    changed setup.py again

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+ 
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
+import os
+def read(fname):
+     with open(fname) as f:
+        return f.read()
 setup(
     name='spa',
     version='0.1',
-    #requires = ['scipy', 'numpy'],
+    #requires = ['scipy(>0.1, !=0.2)', 'numpy(>0.1)'],
    # package_data = { 'speckle': ['gpu/kernels/*.cl']},
-    author = "Run Su",
-    author_email = "runnever@gmail.com",
+    author = "conkr",
+    author_email = "conkr2014@yahoo.com",
     packages=['spa'],
     install_requires=[ 'numpy','scipy' ],
-    description = "python package for scattering and imaging data analysis at Beamline 12.0.2 at the Advanced Light Source",
-    url = "https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/"
+    scripts=['bin/helloworld'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    description = "Python package for scattering and imaging data analysis",
+ #   long_description=read('README.txt'),
+  #  url = "https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/"
 )
   
