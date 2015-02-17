e08b6be3a9d584fb65615720a0c7dba50d4203c1
Run Su <runsu@Eden.local>
2015-02-17 00:17:09 -0800

    setup

--- setup.py
+++ setup.py
@@ -11,17 +11,15 @@ def read(fname):
 setup(
     name='spa',
     version='0.1',
-    #requires = ['scipy(>0.1, !=0.2)', 'numpy(>0.1)'],
-   # package_data = { 'speckle': ['gpu/kernels/*.cl']},
+    requires = ['scipy(>0.1, !=0.2)', 'numpy(>0.1)'],
+    package_data = { 'spa': ['gpu/kernels/*.cl']},
     author = "conkr",
     author_email = "conkr2014@yahoo.com",
     packages=['spa'],
     install_requires=[ 'numpy','scipy' ],
-    scripts=['bin/helloworld'],
     test_suite='nose.collector',
     tests_require=['nose'],
     description = "Python package for scattering and imaging data analysis",
  #   long_description=read('README.txt'),
-  #  url = "https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/"
 )
   
